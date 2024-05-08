3be77000f94ccd6a40f852966a6e518fa12086e4
Bekir <bekiryildirim.2508@gmail.com>
2024-05-08 10:51:53 +0300

    first commit

---
+++ src/main/java/com/pinsoft/interns/CinemaTicketBeApplication.java
@@ -0,0 +1,13 @@
+package com.pinsoft.interns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketBeApplication.class, args);
+	}
+
+}
