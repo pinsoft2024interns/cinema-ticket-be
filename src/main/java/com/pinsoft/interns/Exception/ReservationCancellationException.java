6aa6e8949ec680213c1ce227e8a5f78ceb3912ed
Bekir <bekiryildirim.2508@gmail.com>
2024-05-13 23:38:16 +0300

    added special exceptions

---
+++ src/main/java/com/pinsoft/interns/Exception/ReservationCancellationException.java
@@ -0,0 +1,7 @@
+package com.pinsoft.interns.Exception;
+
+public class ReservationCancellationException extends Exception {
+    public ReservationCancellationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/pinsoft/interns/Exception/SeatFullException.java
@@ -0,0 +1,7 @@
+package com.pinsoft.interns.Exception;
+
+public class SeatFullException extends Exception {
+    public SeatFullException(String message) {
+        super(message);
+    }
+}
