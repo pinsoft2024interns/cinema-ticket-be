f72e4b3264a2961ca69763efc895d2a1c1611168
Bekir <bekiryildirim.2508@gmail.com>
2024-05-30 13:38:17 +0300

    added  dto

--- src/main/java/com/pinsoft/interns/DTO/HallRequest.java
+++ src/main/java/com/pinsoft/interns/DTO/HallRequest.java
@@ -4,16 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 
-public class ReservationUpdateRequest {
+public class HallRequest {
 
-    private Long id;
-    private Long userId;
-    private Long movieId;
+    private String name;
+
+    private int capacity;
+
+    private String screenType;
+
+    private Long cinemaId;
 }
